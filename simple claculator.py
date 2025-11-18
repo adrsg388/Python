9374c67a6532501d157c8e40f1f9bf32f76d1ecb
adrsg388 <adarshpadwal80@gmail.com>
2025-11-18 16:23:37 +0530

    Add files via upload

---
+++ simple claculator.py
@@ -0,0 +1,19 @@
+operation=input("Enter the operation you want (+ - * /):  ")
+num1=int(input("Enter your First Number: "))
+num2=int(input("Enter your Second Number: "))
+
+if operation=='+':
+    print(num1+num2)
+elif operation=='-':
+    print(num1-num2)
+elif operation=='*':
+    print(num1*num2)
+elif operation=='/':
+    print(num1/num2)
+else:
+    print("operation is invalid")
+
+
+
+ 
+
